6c241f62460258694b23a3cee049c6a2144432a1
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-08-24 11:21:10 +0200

    remove other strategies components

--- pages/login.vue
+++ pages/login.vue
@@ -1,25 +1,5 @@
 <template>
   <div>
-    <form @submit.prevent="userLogin">
-      <div>
-        <label>Username</label>
-        <input type="text" v-model="login.username" />
-      </div>
-      <div>
-        <label>Password</label>
-        <input type="text" v-model="login.password" />
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-    <hr class="my-4"/>
-    <button v-on:click="googleLogin()" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
-      Login google
-    </button>
-    <button v-on:click="auth0Login()" class="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">
-      Login auth0
-    </button>
     <button v-on:click="cryptrLogin()" class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded">
       Login Cryptr
     </button>
